957d706bd9944add6fc4956f99b4a9268f16c758
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-19 03:20:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,8 +13,10 @@ def link
   puts  "It's Dangerous To Go Alone! Take This."
 end
 
-def any_phrase
-  puts "Do A Barrel Roll!"
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!"
+  puts phrase
 end
+
 
   
